700e2b8b6830517274f48d2efa79d4bd74df9d88
justin <justin@bassett.green>
2024-04-09 08:50:55 +0000

    initial commit of flask template

---
+++ src/main.py
@@ -0,0 +1,15 @@
+import os
+
+from flask import Flask, send_file
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return send_file('index.html')
+
+def main():
+    app.run(port=int(os.environ.get('PORT', 80)))
+
+if __name__ == "__main__":
+    main()
