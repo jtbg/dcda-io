550807629c7455952ac4a1580e0e618d62971045
kishlay-notabot <kishlay.work1@gmail.com>
2024-04-22 14:30:08 +0530

    add pages + flask routes for webpages

--- cloud-run-app/main.py
+++ cloud-run-app/main.py
@@ -79,6 +79,14 @@ def vote():
 
     return redirect(url_for("index"))
 
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+@app.route("/contact")
+def contact():
+    return render_template("contact.html")
+
 
 if __name__ == "__main__":
     # init_db()  # Initialize database on first run
